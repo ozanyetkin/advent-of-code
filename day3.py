b88598fef8954eae5ed984c39471ba58ada7747d
ozanyetkin <ozan.yetkin.579@gmail.com>
2021-12-03 16:42:22 +0300

    thought debugged but seems like there is another

--- day3.py
+++ day3.py
@@ -50,7 +50,7 @@ print(loser_binary)
 print(int(winner_binary, 2) * int(loser_binary, 2))
 
 # Oh part 2 seems a bit complicated, at least the loop structure is the same, let's see
-def binary_digit(binary_list, index):
+def binary_digit(data, index):
     binary_list = []
     for d in data:
         binary_list.append(int(d[index]))
@@ -59,8 +59,9 @@ def binary_digit(binary_list, index):
     return winning_digit
 
 # Small test for winning binary function
-for i in range(len(data[0])):
-    print(binary_digit(data, i))
+list = ["1010", "1000", "1010", "1000"]
+for i in range(len(list[0])):
+    print(binary_digit(list, i))
 
 # Another function to delete elements according to winning digit
 def binary_remove(binary_list, index, binary_digit):
@@ -71,35 +72,49 @@ def binary_remove(binary_list, index, binary_digit):
     return removed_list
 
 # Small test for binary remove function
-print(binary_remove(data, 0, binary_digit(data, 0)))
+# print(binary_remove(data, 1, binary_digit(data, 0)))
 
 # Now that we have necessary functions, we can implement the most interesting part, recursion
 def oxygen_rate(data):
     for i in range(len(data[0])):
+        print(len(data))
         winning_digit = binary_digit(data, i)
         if len(data) > 1:
             if winning_digit is not None:
                 data = binary_remove(data, i, winning_digit)
             else:
+                print("Equality occured")
                 data = binary_remove(data, i, 1)
-            print(i, data, winning_digit)
+            print(winning_digit, data, i)
         else:
             break
     return data
 
 def carbondioxide_rate(data):
     for i in range(len(data[0])):
-        losing_digit = int(not bool(binary_digit(data, i)))
+        print(len(data))
+        # Told you not to try this at home, causes a bug, never evaluates to None
+        # losing_digit = int(not bool(binary_digit(data, i)))
+        winning_digit = binary_digit(data, i)
+
+        if winning_digit is not None:
+            losing_digit = int(not bool(winning_digit))
+        else:
+            losing_digit = None
+            
         if len(data) > 1:
             if losing_digit is not None:
                 data = binary_remove(data, i, losing_digit)
             else:
+                print("Equality occured")
                 data = binary_remove(data, i, 0)
+            print(losing_digit, data, i)
         else:
             break
     return data
 
 # Calculate oxygen rate and carbondioxide rate, convert to base 10 and multiply for the answer
+print(data)
 oxygen_rate = oxygen_rate(data)[0]
 carbondioxide_rate = carbondioxide_rate(data)[0]
 
