030337a230a6b7a79a5afed341f9e3e9c35d5c5b
ozanyetkin <ozan.yetkin.579@gmail.com>
2021-12-06 16:42:28 +0300

    data read completed for day 4

--- day4.py
+++ day4.py
@@ -1,1 +1,22 @@
 # Advent of Code 2021 | Day 4
+# Reading data gets a bit trickier, we need to split the marked numbers and boards into lists
+from day2 import read_input
+
+data = read_input("day4.txt")
+
+marked_numbers = [int(i) for i in data[0].split(",")]
+# print(marked_numbers)
+board_lines = []
+for i, d in enumerate(data[1:]):
+    if i % 6 != 0:
+        d = d.split(" ")
+        while "" in d:
+            d.remove("")
+        board_lines.append([int(i) for i in d])
+
+# Oh list structure will get me killed in further steps, that's for sure
+boards = []
+for i in range(len(board_lines)):
+    boards.append(board_lines[i:i + 5])
+
+# Now let's find the winning board, prepare yourself squid!
