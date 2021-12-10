957026b6b65860af20bbda3132c2fb261acb2eec
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2021-12-10 15:25:42 +0300

    GPG verified test, day 9

--- day9.py
+++ day9.py
@@ -1,6 +1,17 @@
 # Advent of Code 2021 | Day 9
 # Let's read the data
 from day2 import read_input
+import numpy as np
 
 data = read_input("day9.txt")
-print(data)
+
+# Turn the data into a matrix
+height_matrix = []
+for d in data:
+    rows = []
+    for s in d:
+        rows.append(int(s))
+    height_matrix.append(rows)
+
+height_matrix = np.array(height_matrix)
+print(height_matrix)
