22770edaf8a676327d75ef6be9c7ec9de2a44db9
ozanyetkin <ozan.yetkin.579@gmail.com>
2021-12-10 12:58:11 +0300

    day 7 finished

--- day7.py
+++ day7.py
@@ -14,3 +14,19 @@ for p in positions:
     fuel_consumptions.append(fuel)
 
 print(min(fuel_consumptions))
+
+# Seems like part two is a bit tricky, but not much
+fuel_consumptions = []
+for p in positions:
+    fuel = sum([(abs(p - i)) * (abs(p - i) + 1) // 2 for i in positions])
+    fuel_consumptions.append(fuel)
+
+print(min(fuel_consumptions))
+
+data = [int(x) for x in open("day7.txt").read().strip().split(",")]
+
+pt1 = {x: sum([abs(x - z) for z in data]) for x in range(0, max(data) + 1)}
+print(f"Part 1: {min(pt1.values())}")
+
+pt2 = {x: sum([abs(x - z) / 2 * (2 + (abs(x - z) - 1)) for z in data]) for x in range(0, max(data) + 1)}
+print(f"Part 2: {int(min(pt2.values()))}")
