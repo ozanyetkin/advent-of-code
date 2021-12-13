cc3a803619d97c3b1d8e74774dba88d7023583e5
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2021-12-13 15:49:11 +0300

    day 10 part 1 partially finished

--- day10.py
+++ day10.py
@@ -19,3 +19,36 @@ def is_valid(input_string):
 
 for d in data:
     print(is_valid(d))
+
+# Let's find the syntax error scores
+def syntax_points(input_string):
+    while "{}" in input_string or "[]" in input_string or "()" in input_string or "<>" in input_string:
+        input_string = input_string.replace("{}", "")
+        input_string = input_string.replace("[]", "")
+        input_string = input_string.replace("()", "")
+        input_string = input_string.replace("<>", "")
+
+    point_3 = input_string.find(")") if input_string.find(")") >= 0 else 99
+    point_57 = input_string.find("]") if input_string.find("]") >= 0 else 99
+    point_1197 = input_string.find("}") if input_string.find("}") >= 0 else 99
+    point_25137 = input_string.find(">") if input_string.find(">") >= 0 else 99
+
+    if min(point_3, point_57, point_1197, point_25137) == point_3:
+        return 3
+    elif min(point_3, point_57, point_1197, point_25137) == point_57:
+        return 57
+    elif min(point_3, point_57, point_1197, point_25137) == point_1197:
+        return 1197
+    elif min(point_3, point_57, point_1197, point_25137) == point_25137:
+        return 25137
+    elif min(point_3, point_57, point_1197, point_25137) == 99:
+        return 0
+    else:
+        return "Something is wrong"
+
+total = 0
+for d in data:
+    if is_valid(d) == False:
+        total += syntax_points(d)
+
+print(total)
